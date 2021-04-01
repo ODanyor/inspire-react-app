4c98ff5c4e2a4db3a826738eea12198b6613ab2b
ODanyor <doniyorgit@gmail.com>
2021-04-01 18:13:54 +0500

    new updates | check for withSection hoc

--- src/shared/hocs/withSection.tsx
+++ src/shared/hocs/withSection.tsx
@@ -4,8 +4,8 @@ import styles from 'pages/HomePage/home.module.sass';
 
 export default function WithSection(WrappedComponent: React.FC<SectionProps>) {
   return forwardRef((props: SectionProps, ref: Ref<HTMLDivElement>) => (
-    <div className={styles.section} ref={ref}>
+    <section className={styles.section} ref={ref}>
       <WrappedComponent {...props} />
-    </div>
+    </section>
   ));
 }
